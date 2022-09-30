3e8957dfa7ef7e82940b8c26a58669ba17fae4d2
stargazer741 <102627303+stargazer741@users.noreply.github.com>
2022-10-01 00:50:09 +0300

    day 13

--- 13 - Random Choice Picker/script.js
+++ 13 - Random Choice Picker/script.js
@@ -5,9 +5,10 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
-  if(e.key === 'Enter') {
+
+  if (e.key === "Enter") {
     setTimeout(() => {
-        e.target.value = "";
+      e.target.value = "";
     }, 10);
 
     randomSelect();
@@ -19,43 +20,45 @@ function createTags(input) {
     .split(",")
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
-  
 
-    tagsEl.innerHTML = "";
+  tagsEl.innerHTML = "";
 
-    tags.forEach(tag => {
-        const tagEl = document.createElement("span");
-        tagEl.classList.add("tag");
-        tagEl.innerText = tag;
-        tagsEl.appendChild(tagEl);
-    });
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
 
 function randomSelect() {
-    const times = 30;
-    
-    const interval = setInterval(() => {
-        const randomTag = pickRandomTag();
-        highlightTag(randomTag);
+  const times = 30;
 
-        setTimeout(() => {
-            unHighlightTag(randomTag);
-        }, 100);
-    }, 100);
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
 
     setTimeout(() => {
-        clearInterval(interval);
-        setTimeout(() => {
-            const randomTag = pickRandomTag();
+        unHighlightTag(randomTag);
+    }, 100);
 
-            highlightTag(randomTag);
-        }, 100);
-    }, times * 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
 }
 
 function pickRandomTag() {
-    const tags = document.querySelectorAll(".tag");
-    return tags(Math.floor(Math.random() * tags.length));
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
 }
 
 function highlightTag(tag) {
