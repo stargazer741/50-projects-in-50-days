1e0d9d98e5e54619fdd4cb18fac7bee073b63c7a
stargazer741 <102627303+stargazer741@users.noreply.github.com>
2022-10-20 00:42:41 +0300

    done

--- 20 - Drawing App/script.js
+++ 20 - Drawing App/script.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeElement = document.getElementById("size");
+const colorElement = document.getElementById("color");
+const clearElement = document.getElementById("clear");
+
 let size = 20;
-let color = "black";
+colorElement.value = "black";
+let color = colorElement.value;
 let isPressed = false;
 
 let x;
@@ -22,6 +29,19 @@ canvas.addEventListener("mouseup", (e) => {
   y = undefined;
 });
 
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
+
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
@@ -34,8 +54,38 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
 
-drawCircle(100, 100);
+function updateSizeOnScreen() {
+  sizeElement.innerText = size;
+}
+
+increaseBtn.addEventListener("click", () => {
+  size = size + 5;
+
+  if (size > 50) {
+    size = 50;
+  }
+
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size = size - 5;
+
+  if (size < 5) {
+    size = 5;
+  }
+
+  updateSizeOnScreen();
+});
+
+colorElement.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+clearElement.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
