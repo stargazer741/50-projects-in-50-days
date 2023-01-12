c7a0ad924cf73effaf2414325444335a6ed9781a
stargazer741 <102627303+stargazer741@users.noreply.github.com>
2023-01-12 23:50:11 +0300

    day 31

---
+++ 31 - Hoverboard/script.js
@@ -0,0 +1,33 @@
+const container = document.getElementById("container");
+const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+const SQUARES = 500;
+
+for (let index = 0; index < SQUARES; index++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseout", () => {
+    removeColor(square);
+  });
+
+  container.appendChild(square);
+}
+
+function setColor(e) {
+  const color = getRandomColor();
+  e.style.background = color;
+  e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(e) {
+    e.style.background = '#1d1d1d';
+    e.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
